516f179e48a388990e83aa10c64bcf9ae15d74f8
Louis Gentil <louisgentil89@gmail.com>
2023-09-20 23:39:41 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const server = Bun.serve({
   hostname: "::",
   port: process.env.PORT ?? 3000,
   fetch(request) {
-    return new Response(`Welcome to Bun! ${Bun.env.MONGO_URL}`);
+    return new Response(`Welcome to Bun!`);
   },
 });
 
