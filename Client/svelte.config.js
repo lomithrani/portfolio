f75ffaee88129493949cf37821ef074ba4d449b3
Louis <jlouisgentil89@gmail.com>
2023-09-23 20:13:21 +0200

    retry bun adapter

--- Client/svelte.config.js
+++ Client/svelte.config.js
@@ -1,5 +1,4 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-node";
+import adapter from "svelte-adapter-bun";
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
