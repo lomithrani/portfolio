f9aed3f223bf172f6232e0c60be284eac6ca0103
Louis <louisgentil89@gmail.com>
2023-09-23 22:36:02 +0200

    Switch to url provided from env

--- Client/src/services/portfolio.ts
+++ Client/src/services/portfolio.ts
@@ -1,6 +1,8 @@
 import { edenTreaty } from '@elysiajs/eden'
 import type { Portfolio } from '../../../Server'
 
-const api = edenTreaty<Portfolio>("http://localhost:3000")
+import { PUBLIC_PORTFOLIO_API_URL } from '$env/static/public'
+
+const api = edenTreaty<Portfolio>(PUBLIC_PORTFOLIO_API_URL)
 
 export { api }
