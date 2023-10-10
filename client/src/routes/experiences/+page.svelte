7f64e0b3a2ff61b64c242cb631a5ca2cd8f634dc
Louis <louisgentil89@gmail.com>
2023-10-10 07:54:24 +0200

    Add Filtering

--- client/src/routes/experiences/+page.svelte
+++ client/src/routes/experiences/+page.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
-	import Experiences from './Experiences.svelte';
+	import Experience from '$components/Experience.svelte';
 	import type { PageData } from './$types';
 	import Filters from './Filters.svelte';
+	import type { Experience as ExperienceModel } from 'portfolio-api/models';
 	export let data: PageData;
+
+	let filters: { softSkills: Set<string>; hardSkills: Set<string> } = {
+		softSkills: new Set(),
+		hardSkills: new Set()
+	};
+	let experiences: ExperienceModel[] | null = null;
+
+	$: if (data?.experiences) filterExperiences();
+	$: if (filters) filterExperiences();
+
+	const filterExperiences = () => {
+		console.log('filter experiences');
+		if (filters.softSkills.size === 0 && filters.hardSkills.size === 0) {
+			return (experiences = data?.experiences);
+		}
+
+		if (data?.experiences === null) {
+			return (experiences = null);
+		}
+
+		return (experiences = data.experiences.filter((experience) =>
+			experience.projects.some(
+				(project) =>
+					project.softSkills.some((softSkill) => filters.softSkills.has(softSkill.name)) ||
+					project.hardSkills.some((hardSkill) => filters.hardSkills.has(hardSkill.name))
+			)
+		));
+	};
 </script>
 
 <svelte:head>
@@ -11,11 +40,13 @@
 </svelte:head>
 
 <div class="text-column">
-	{#if !data?.experiences}
+	{#if !experiences}
 		Loading
 	{/if}
-	{#if data?.experiences}
-		<Filters experiences={data.experiences} />
-		<Experiences experiences={data.experiences} />
+	{#if experiences}
+		<Filters bind:filters {experiences} />
+		{#each experiences as experience}
+			<Experience {experience} />
+		{/each}
 	{/if}
 </div>
--- client/src/routes/experiences/Experiences.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Experience from '$components/Experience.svelte';
-	import type { Experience as ExperienceModel } from 'portfolio-api/models';
-	export let experiences: ExperienceModel[];
-</script>
-
-{#each experiences as experience}
-	<Experience {experience} />
-{/each}
