fd13ca1f061540b152ee8e53d6370ba48f0d8f76
Louis <louisgentil89@gmail.com>
2023-11-03 00:21:41 +0100

    add mongourl

--- server/tests/seedDb.ts
+++ server/tests/seedDb.ts
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
 import { Domain, User } from "../models/database";
+
+await mongoose.connect(Bun.env.MONGO_URL ?? '');
 
 const user = new User({
   email: 'louisgentil89@gmail.com',
