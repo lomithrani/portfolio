a8ba68cbc9554d3006167cbd3451c13c32fdb5ff
Louis <louisgentil89@gmail.com>
2023-11-02 08:10:31 +0100

    - fixes few tests - added github action - fixes duplicate in modal - added health endpoint to server

--- client/src/services/tests/portfolio.test.ts
+++ client/src/services/tests/portfolio.test.ts
@@ -1,7 +1,6 @@
 import { test, expect } from 'bun:test'
 import { edenTreaty } from '@elysiajs/eden'
 import type { Portfolio } from 'portfolio-api'
-import { ExperienceType } from 'portfolio-common';
 
 const api = edenTreaty<Portfolio>("");
 
@@ -10,5 +9,4 @@ test("edenTreaty should have correct methods", async () => {
   expect(api.login.post).toBeDefined()
   expect(api.domain[':name'].get).toBeDefined()
   expect(api.experiences.post).toBeDefined()
-  let { data, error } = await api.domain[''].get();
 })
--- client/tests/test.ts
+++ client/tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('hire page has expected h1 when unathorized', async ({ page }) => {
-	await page.goto('/hire');
-	await expect(page.getByRole('heading', { name: 'Unauthorized' })).toBeVisible();
+test('landing page should be my profile for now', async ({ page }) => {
+	await page.goto('/');
+	await expect(new URL(page.url()).pathname).toBe("/louis.gentil/experiences");
 });
